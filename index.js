81b02a3ff19739f2bd1fb5628115929c4fc8fdbe
Karanpreet Singh <83339856+karan150602@users.noreply.github.com>
2024-02-23 10:40:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const PORT = process.env.PORT || 8080;
 const API_KEY = process.env.API_KEY;
 
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-/*app.use(express.json({ limit: "50mb" }));
-app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
-);*/
 
 app.get("/", (req, res) => {
   res.render("index", { weather: null, error: null });
@@ -22,8 +19,7 @@ app.get("/weather", async (req, res) => {
   if (!city) {
     return res.status(400).json({ message: "City is Required!!" });
   }
-  let weather,
-    error = null;
+  let weather, error = null;
   try {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`;
     const response = await axios.get(apiUrl);
